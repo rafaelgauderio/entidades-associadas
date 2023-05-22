ff3734f8509fff96ea21bc75682796da54a98683
Rafael de Luca <deluca1712@gmail.com>
2023-05-22 13:45:21 -0300

    Initial Seed and application.properties

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_group (name) VALUES ('Alkali Metal');
+INSERT INTO tb_group (name) VALUES ('Alkaline Earth');
+INSERT INTO tb_group (name) VALUES ('Transation Metal');
+INSERT INTO tb_group (name) VALUES ('Semi Metal');
+
+INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Lithium',3,1);
+INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Sodium',11,1);
+INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Magnesium',12,2);
+INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Calcium',20,2);
+INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Titanium',22,3);
