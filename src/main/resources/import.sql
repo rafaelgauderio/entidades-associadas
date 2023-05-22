90dd5a3f37a61dad06022491d899cebee8753058
Rafael de Luca <deluca1712@gmail.com>
2023-05-22 15:23:10 -0300

    Initial seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -8,3 +8,18 @@ INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Sodium'
 INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Magnesium',12,2);
 INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Calcium',20,2);
 INSERT INTO tb_quemical_element (name, atomic_number, group_id) VALUES ('Titanium',22,3);
+
+INSERT INTO tb_category (name) VALUES ('Televisão');
+INSERT INTO tb_category (name) VALUES ('Cozinha');
+INSERT INTO tb_category (name) VALUES ('banheiro');
+
+INSERT INTO tb_product (name, price) VALUES ('Smart TV Sansung 50 polegadas', 1990.5);
+INSERT INTO tb_product (name, price) VALUES ('Fogão 5 bocas', 499.0);
+INSERT INTO tb_product (name, price) VALUES ('Geladeira', 1250.0);
+INSERT INTO tb_product (name, price) VALUES ('Tapete antiderrapante', 60.0);
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1,1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2,2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3,2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4,2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4,3);
